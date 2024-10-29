dbf03b8fd3f33ff51d0fdc3712e9221e280cc8fd
Jost1nVx7 <jostinjd2002@gmail.com>
2024-10-29 09:18:32 -0500

    Mavenproyec2

---
+++ src/main/java/espoch/edu/ec/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.mavenproject2;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+}
